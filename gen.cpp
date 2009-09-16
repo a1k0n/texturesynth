9f9ab068112c78b6b96626d02b17426a5edf439b
Andy Sloane <andy@a1k0n.net>
2009-09-16 14:25:39 -0500

    cleaned it up a bunch; added support for non-wrapping source textures; not sure whether it really works

--- gen.cpp
+++ gen.cpp
@@ -6,6 +6,15 @@
 unsigned *dstim_rgb, *srcim_rgb;
 int srcw, srch, dstw, dsth;
 
+// define this iff your source texture tiles cleanly
+#define SRCWRAP
+// neighborhood for synthesis
+#define NEIGHBORHOOD 4
+// number of seam-rows to keep constant between different interchangable tiles
+#define KEEPROWS 2
+// generator iterations
+#define ITERATIONS 16
+
 #define SRC(i,j) srcim_rgb[(i)+(j)*srcw]
 #define DST(i,j) dstim_rgb[(i)+(j)*dstw]
 
@@ -20,12 +29,20 @@ unsigned nn_diff(int dstx, int dsty, int srcx, int srcy, int Nsize)
 {
 	unsigned diff=0;
 	for(int j=-Nsize;j<=Nsize;j++) {
+#ifdef SRCWRAP
 		int sy = (srch+srcy+j)%srch;
+#else
+		int sy = srcy+j;
+#endif
 		int dy = (dsth+dsty+j)%dsth;
 		for(int i=-Nsize;i<=Nsize;i++) {
 			if(i == 0 && j == 0)
 				continue;
+#ifdef SRCWRAP
 			int sx = (srcw+srcx+i)%srcw;
+#else
+			int sx = srcx+i;
+#endif
 			int dx = (dstw+dstx+i)%dstw;
 			int s = SRC(sx,sy);
 			int d = DST(dx,dy);
@@ -43,8 +60,13 @@ unsigned nn_diff(int dstx, int dsty, int srcx, int srcy, int Nsize)
 unsigned nn_search(int dstx, int dsty, int &srcx, int &srcy, int Nsize)
 {
 	unsigned bestdiff = ~0;
+#ifdef SRCWRAP
 	for(int y=0;y<srch;y++) {
 		for(int x=0;x<srcw;x++) {
+#else
+	for(int y=Nsize;y<srch-Nsize;y++) {
+		for(int x=Nsize;x<srcw-Nsize;x++) {
+#endif
 			unsigned diff = nn_diff(dstx,dsty, x,y, Nsize);
 			if(diff < bestdiff) {
 				bestdiff = diff;
@@ -72,7 +94,7 @@ unsigned gen_step(int offset)
 	for(int j=offset;j<dsth;j++) {
 		for(int i=offset;i<dstw;i++) {
 			int x=0,y=0;
-			err += nn_search(i,j, x,y, 4);
+			err += nn_search(i,j, x,y, NEIGHBORHOOD);
 			DST(i,j) = SRC(x,y);
 		}
 	}
@@ -91,10 +113,10 @@ int main()
 	dstim_rgb = new unsigned[dstw*dsth];
 
 	for(int version=0;version<8;version++) {
-		gen_init(version==0 ? 0 : 2);
-		for(int iterations=0;iterations<10;iterations++) {
+		gen_init(version==0 ? 0 : KEEPROWS);
+		for(int iterations=0;iterations<ITERATIONS;iterations++) {
 			printf("ver %d iteration %d: ", version, iterations); fflush(stdout);
-			printf("%u\n", gen_step(version==0 ? 0 : 2));
+			printf("%u\n", gen_step(version==0 ? 0 : KEEPROWS));
 		}
 
 		char buf[20];
